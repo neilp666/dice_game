ea50c83569e5dbf6c5b83163ffacc28f8e7cbd61
neil patel <neil@myindianweddingapp.com>
2019-03-07 10:13:43 +0000

    Added roll the dice event listener function

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  
+    var dice = Math.floor(Math.random() * 6) + 1
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'assets/dice-' + dice + '.png';
+    
+
+});
