3b91c0127c478f5a7343e685573108fc30d7c18c
neil patel <neil@myindianweddingapp.com>
2019-03-07 09:10:40 +0000

    Setup variables & arrays for scores

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
